5f7b27a66c5f53fadbda4508e9d3ba54140fd6b5
EunJung803 <aimee3322@gmail.com>
2024-04-24 15:50:12 +0900

    Class 정의 필드 #finish, 클래스메서드 #1

---
+++ chapter03/src/main/java/chapter03/ArrayUtilTeset.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+import java.util.Arrays;
+
+public class ArrayUtilTeset {
+
+	public static void main(String[] args) {
+		
+		int[] a = {10, 20, 30, 40};
+		
+		double[] d = ArrayUtil.intToDouble(a);
+		System.out.println(Arrays.toString(d));
+
+	}
+
+}
---
+++ chapter03/src/main/java/chapter03/StaticMethod.java
@@ -0,0 +1,9 @@
+package chapter03;
+
+public class StaticMethod {
+	
+	int n;			// 인스턴스 변수
+	static int m;	// 클래스 변수
+	
+
+}
