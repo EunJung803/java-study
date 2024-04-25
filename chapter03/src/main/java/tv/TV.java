e82b544c2b4a8e027c6ac3497a59243ea90d8702
EunJung803 <aimee3322@gmail.com>
2024-04-25 16:47:44 +0900

    2.2 클래스와 다형성 - 실습6 TV문제

--- chapter03/src/main/java/tv/TV.java
+++ chapter03/src/main/java/tv/TV.java
@@ -19,4 +19,61 @@ public class TV {
 		this.volume = volume;
 		this.power = power;
 	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public boolean isPower() {
+		return power;
+	}
+
+	public void power(boolean power) {
+		this.power = power;
+	}
+
+	public void volume(int volume) {
+		if(volume > 100) {
+			this.volume = 0;
+		}
+		else if(volume < 0) {
+			this.volume = 100;
+		}
+	}
+
+	public void channel(int channel) {
+		if(channel > 255) {
+			this.channel = 1;
+		}
+		else if(channel < 1) {
+			this.channel = 255;
+		}
+	}
+
+	public void channel(boolean b) {
+		if(b) {
+			this.channel += 1;
+			channel(channel);
+		}
+		if(b==false) {
+			this.channel -= 1;
+			channel(channel);
+		}
+		
+	}
+
+	public void volume(boolean b) {
+		if(b) {
+			this.volume += 1;
+			volume(volume);
+		}
+		if(b==false) {
+			this.volume -= 1;
+			volume(volume);
+		}
+	}
 }
