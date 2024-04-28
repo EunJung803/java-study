082d2e049ee951a228cba68879a699fdd7f5f9da
EunJung803 <aimee3322@gmail.com>
2024-04-29 00:13:25 +0900

    Practice05 #2 Refactor

--- practice05/src/main/java/prob2/SmartPhone.java
+++ practice05/src/main/java/prob2/SmartPhone.java
@@ -2,24 +2,34 @@ package prob2;
 
 public class SmartPhone extends MusicPhone {
 	
+//	@Override
+//	public void execute(String function) {
+//		if("음악".equals(function)) {
+//			playMusic();
+//		}
+//		else if("앱".equals(function)) {
+//			app();
+//		}
+//		else {
+//			super.execute(function);
+//		}
+//	}
+	
 	@Override
 	public void execute(String function) {
-		if("음악".equals(function)) {
-			music();
-		}
-		else if("앱".equals(function)) {
+		if ("앱".equals(function)) {
 			app();
+			return;
 		}
-		else {
-			super.execute(function);
-		}
+		super.execute(function);
 	}
 	
-	private void music() {
+	@Override
+	protected void playMusic() {
 		System.out.println("다운로드해서 음악재생");
 	}
 	
-	private void app() {
+	protected void app() {
 		System.out.println("앱실행");
 	}
 }
