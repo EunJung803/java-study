f01ca6baa2d333467290d0b813752945661d56d9
EunJung803 <aimee3322@gmail.com>
2024-04-29 10:19:15 +0900

    String method

--- chapter04/src/main/java/chapter04/StringTest03.java
+++ chapter04/src/main/java/chapter04/StringTest03.java
@@ -33,6 +33,34 @@ public class StringTest03 {
 		}
 		String s3 = sb.toString();
 		
+		
+		// String method들
+		String s4 =  "aBcABCabcAbc";
+		System.out.println(s4.length());
+		System.out.println(s4.charAt(2));
+		System.out.println(s4.indexOf("abc"));			// 가장 먼저 등장한 인덱스
+		System.out.println(s4.indexOf("abc", 7));		// 시작 인덱스 붙이기 가능, 없으면 -1
+		System.out.println(s4.substring(3));			// 인덱스 3부터 슬라이싱
+		System.out.println(s4.substring(3, 5));			// 인덱스 3에서 5(포함X)까지 자름
+		
+		String s5 = "      ab        cd         ";
+		String s6 = "efg,hij,klm,nop,qrs";
+		
+		String s7 = s5.concat(s6);
+		System.out.println(s7);												// 두 String 합치기
+		System.out.println("----" + s5.trim() + "----");					// 겉에 있는 공백 삭제
+		System.out.println("----" + s5.replaceAll(" ", "") + "----");		// 해당 문자열 모두 replace
+		
+		String[] tokens = s6.split(",");		// 해당 문자열을 기준으로 잘라내어 배열에 담김
+		for(String s : tokens) {
+			System.out.println(s);
+		}
+		
+		String[] tokens2 = s6.split(" ");		// 해당 문자열을 기준으로 잘라낼 수 없는 경우는 -> 통째로 안잘리고 들어감 배열에
+		for(String s : tokens2) {
+			System.out.println(s);
+		}
+		
 	}
 
 }
