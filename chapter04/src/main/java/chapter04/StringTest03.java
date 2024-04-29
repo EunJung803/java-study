2c95e5279f153d5fac7b03f3e0d5309bf872726e
EunJung803 <aimee3322@gmail.com>
2024-04-29 09:51:48 +0900

    3. StringBuffer 클래스

--- chapter04/src/main/java/chapter04/StringTest03.java
+++ chapter04/src/main/java/chapter04/StringTest03.java
@@ -11,7 +11,28 @@ public class StringTest03 {
 						.append(17)
 						.toString();
 		
-		System.out.println(s1);
+//		String s2 = new StringBuilder("Hello ")
+//						.append("World")
+//						.append(" java ")
+//						.append(17)
+//						.toString();
+
+//		System.out.println(s1);
+//		System.out.println(s2);
+		
+		String s2 = "";
+		for(int i = 0; i < 1000000; i++) {
+			// s2 += "h";
+			// s2 = new StringBuffer(s2).append("h").toString();
+		} 
+		
+		StringBuffer sb = new StringBuffer("");
+		
+		for(int i = 0; i < 1000000; i++) {
+			sb.append("h");
+		}
+		String s3 = sb.toString();
+		
 	}
 
 }
