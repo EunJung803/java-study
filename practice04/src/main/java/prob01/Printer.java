b74f26139908991d3a0a87c3729336bd29080877
EunJung803 <aimee3322@gmail.com>
2024-04-29 11:39:04 +0900

    제너릭 타입 메서드

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,20 +2,25 @@ package prob01;
 
 public class Printer {
 	
-	public void println(int i) {
-		System.out.println(i);
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+//
+//	public void println(String string) {
+//		System.out.println(string);
+//	}
+	
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
-
-	public void println(boolean b) {
-		System.out.println(b);
-	}
-
-	public void println(double d) {
-		System.out.println(d);
-	}
-
-	public void println(String string) {
-		System.out.println(string);
-	}
+	
 
 }
--- practice04/src/main/java/prob01/PrinterTest.java
+++ practice04/src/main/java/prob01/PrinterTest.java
@@ -5,6 +5,7 @@ public class PrinterTest {
 	public static void main(String[] args) {
 		Printer printer = new Printer();
 
+//		printer.<Integer>println( 10 );
 		printer.println( 10 );
 		printer.println( true );
 		printer.println( 5.7 );
