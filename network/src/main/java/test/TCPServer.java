a40477c53ea65874c1c7e7727695e07b9172308f
EunJung803 <aimee3322@gmail.com>
2024-05-01 15:20:56 +0900

    TCP Server #finish

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -1,6 +1,8 @@
 package test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -24,7 +26,54 @@ public class TCPServer {
 			// 3. Accept
 			Socket socket = serverSockert.accept();		// 코드가 더 진행안됨 (blocking) -> 클라이언트의 요청을 기다림
 			
-			System.out.println("연결됨");
+			try {
+			
+				// 반대편(클라이언트 쪽)에 있는 애의 socket 주소 받아오기
+				InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();	// 캐스팅
+				String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+				int remotePort = inetRemoteSocketAddress.getPort();
+				System.out.println("[server] connected by clinet[" + remoteHostAddress + ":" + remotePort + "]");
+			
+				// 4. 소켓 안에 있는 IO Stream 받아오기
+				InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
+				
+				// 5. 데이터 읽기
+				while(true) {
+					byte[] buffer = new byte[256];
+					int readByteCount = is.read(buffer);	// blocking
+					
+					if(readByteCount == -1) {		// 정상적인 종료 (클라이언트에서 종료)
+						System.out.println("[server] closed by client");	// 클라이언트가 정상적으로 종료 (close() 호출)
+						break;
+					}
+					
+					// String으로 엔코딩
+					String data = new String(buffer, 0, readByteCount, "UTF-8");
+					
+					System.out.println("[server] received:");
+					
+					// 6. 데이터 쓰기
+					os.write(data.getBytes("utf-8"));
+					
+					
+				}
+				
+				
+			} catch (IOException e) {
+				
+				System.out.println("[server] error : " + e);
+				
+			} finally {
+				try {
+					if(socket != null && !socket.isClosed()) {
+						socket.close();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+				
 			
 		} catch (IOException e) {
 			
